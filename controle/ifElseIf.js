3d8526155c71febde0a619d266f3c5539c10dc3e
sergioacamargo <camargo.alves@gmail.com>
2020-05-14 11:30:53 -0300

    Atualização de Exercicios

---
+++ controle/ifElseIf.js
@@ -0,0 +1,20 @@
+Number.prototype.entra = function(inicio, fim){
+    return this >= inicio && this <= fim
+}
+
+
+const imprimirResultado = function(nota){
+    if(nota.entra(9, 10)){
+        console.log('Quadro e honra!')
+    }else if(nota.entra(7, 9.99)){
+        console.log('Aprovado!')
+    }else if(nota.entra(4, 6.99)){
+        console.log('Recuperação!')
+    }else if(nota.entra(0, 3.99)){
+        console.log('Aluno Reprovado!')
+    }else{
+        console.log('Nota Invalida')
+    }
+    console.log('Fim!')
+} 
+imprimirResultado(9)  
