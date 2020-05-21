e6957e58a7dcb3ed9eabff130f6a1bc610ccc5fe
sergioacamargo <camargo.alves@gmail.com>
2020-05-21 11:58:45 -0300

    Atualizando Exercicios

---
+++ objeto/heranca2.js
@@ -0,0 +1,44 @@
+// Cadeia de protótipos (prototype chain)
+Object.prototype.attr0 = '0' //não faça isso em casa
+
+const avo = {attr1:'A'}
+const pai = {__proto__: avo, attr2: 'B'}
+const filho = {__proto__: pai, attr3: 'C'}
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3)
+
+const carro = {
+    velAtual: 0,
+    velMax: 200,
+    acelearMais(delta) {
+        if (this.velAtual + delta <= this.velMax) {
+            this.velAtual +=delta
+        }else{
+            this.velAtual = this.velMax
+        }
+    },
+    status (){
+        return `${this.velAtual}Km/h de ${this.velMax}`
+    }
+}
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324 //shadowing    
+}
+const volvo = {
+    modelo: 'V40',
+    status() {
+        return `${this.modelo}: ${super.status()}`
+    }   
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari)
+console.log(volvo)
+
+volvo.acelearMais(100)
+console.log(volvo.status())
+
+ferrari.acelearMais(300)
+console.log(ferrari.status())
